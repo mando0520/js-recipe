cfafbaf60aef4c11f4aa98d215fab0b62e42b4cf
mando-narin <y2pgftpe@keio.jp>
2021-04-22 18:20:58 +0900

    [ストップウォッチ]を学習しました。

--- 3_semi/week1/main.js
+++ 3_semi/week1/main.js
@@ -1,5 +1,26 @@
-const figure = document.getElementById("figure")
+const display = document.getElementById("display")
+const button = document.getElementById("button")
 
-figure.onclick = function() {
-  figure.classList.toggle("rounded")
+let count = 0
+
+const countUp = function() {
+  // count を更新
+  count += 1
+  // count を秒単位にして表示
+  display.textContent = count / 100
+}
+
+let id = null
+
+button.onclick = function() {
+    if (id === null) {
+        // start
+        id = setInterval(countUp, 10)
+        button.textContent = "stop"
+    } else {
+        // stop
+        clearInterval(id)
+        id = null
+        button.textContent = "start"
+    }
 }
